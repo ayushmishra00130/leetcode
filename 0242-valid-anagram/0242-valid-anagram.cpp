e9223cabb7ae98de3cb977d8b2ab920bfcbd3c70
ayushmishra00130 <ayushmishra00130@gmail.com>
2026-08-09 00:37:35 +0530

    Time: 0 ms (100%), Space: 9.6 MB (76.48%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,9 +1,20 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
+        if(s.length() != t.length()) return false;
 
-        return (s == t ? 1 : 0);
+        int arr[26] = {0};
+
+        for(int i=0;i<s.length();i++){
+            arr[s[i] - 'a']++;
+        }
+
+        for(int i=0;i<t.length();i++){
+            if(arr[t[i] - 'a'] == 0) return false;
+
+            arr[t[i] - 'a']--;
+        }
+
+        return true;
     }
 };
