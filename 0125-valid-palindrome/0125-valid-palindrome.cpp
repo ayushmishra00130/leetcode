69c799510d1757e4b048bb5d88ccab84d971cfb6
ayushmishra00130 <ayushmishra00130@gmail.com>
2026-08-11 23:23:13 +0530

    Time: 3 ms (28.6%), Space: 10 MB (54.19%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        if(s.length() == 0){
+            return true;
+        }
+
+        int n = s.length();
+
+        int i = 0, j = n-1;
+        while(i < j){
+            if(!isalnum(s[i])) i++;
+
+            else if(!isalnum(s[j])) j--;
+
+            else{
+                if(tolower(s[i]) != tolower(s[j])) return false;
+                i++ , j--;
+            }
+        }
+        return true;
+    }
+};
